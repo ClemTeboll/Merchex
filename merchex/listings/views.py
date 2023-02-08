29775833b061d2889e7797ca17b6a4ec5a3c2a5d
Clément <clement.llobet@tutanota.com>
2023-02-08 14:58:40 +0100

    add new views

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hello(request):
+    return HttpResponse("<h1>Hello Django!</h1>")
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+def listing(request):
+    return HttpResponse('<h1>Listings</h1> <p>Liste complète</p>')
+
+def contact_us(request):
+    return HttpResponse('<h1>Contactez-nous</h1> <p>Veuillez nous contacter <a href="">ici</a> !</p>')
+
