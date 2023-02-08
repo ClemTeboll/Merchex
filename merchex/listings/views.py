7a18ea85f322e3fff32792bac245adffbb680328
Clément <clement.llobet@tutanota.com>
2023-02-08 17:55:57 +0100

    create templates and manage html files

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,14 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Band, Announcement
 
 def hello(request):
-    return HttpResponse("<h1>Hello Django!</h1>")
+    bands = Band.objects.all()
+    return render(request,
+                  'listings/hello.html',
+                  {'bands': bands})
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
 def listing(request):
-    return HttpResponse('<h1>Listings</h1> <p>Liste complète</p>')
+    announcements = Announcement.objects.all()
+    return render(request,
+           'listings/listing.html',
+           {'announcements': announcements})
 
 def contact_us(request):
     return HttpResponse('<h1>Contactez-nous</h1> <p>Veuillez nous contacter <a href="">ici</a> !</p>')
